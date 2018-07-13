335fffc3601d381b46f292fc558fadd463659464
Kent <le6790@gmail.com>
2018-07-13 09:00:38 -0600

    Implemented adding expenses

--- budgetProject/budget/urls.py
+++ budgetProject/budget/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
     path('', views.project_list, name='list'),
+    path('add', views.ProjectCreateView.as_view(), name='add'),
     path('<slug:project_slug>', views.project_detail, name ='detail')
 ]
--- budgetProject/budget/views.py
+++ budgetProject/budget/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project
-
+from django.http import HttpResponseRedirect
+from .models import Project, Category, Expense
+from django.views.generic import CreateView
+from django.utils.text import slugify
+from .forms import ExpenseForm
 # Create your views here.
 
 def project_list(request):
@@ -8,7 +11,51 @@ def project_list(request):
 
 def project_detail(request, project_slug):
     #fetching the correct project
-
     project = get_object_or_404(Project, slug = project_slug)
-    expense_list = project.expenses.all()
-    return render(request,'budget/project-detail.html', {'project': project, 'expense_list': expense_list})
+
+    if request.method == 'GET':
+        category_list = Category.objects.filter(project=project)
+        return render(request,'budget/project-detail.html', {'project': project, 'expense_list':project.expenses.all(), 'category_list' : category_list})
+
+    elif request.method == 'POST':
+        #process the form
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            amount = form.cleaned_data['amount']
+            category_name = form.cleaned_data['category']
+
+            category = get_object_or_404(Category, project=project, name = category_name)
+
+            Expense.objects.create(
+                project = project,
+                title = title,
+                amount = amount,
+                category=category
+            ).save()
+
+
+    return HttpResponseRedirect(project_slug)
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'budget/add-project.html'
+    fields = ('name', 'budget')
+
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+
+        categories = self.request.POST['categoriesString'].split(',')
+        for category in categories:
+            Category.objects.create(
+                project= Project.objects.get(id=self.object.id),
+                name = category
+            ).save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return slugify(self.request.POST['name'])
